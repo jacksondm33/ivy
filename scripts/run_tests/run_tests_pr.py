18cfae77c792cda92219e8d6e343b9dbdedbe9bf
Jackson McClintock <jacksondm33@gmail.com>
2024-04-04 02:18:18 -0400

    fix: include CI database in main branch tests

--- scripts/run_tests/run_tests_pr.py
+++ scripts/run_tests/run_tests_pr.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
                 sys.stdout.flush()
                 command = f'docker run --rm -v "$(pwd)":/ivy -v "$(pwd)"/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest --tb=short {test_path} --skip-trace-testing --skip-trace-testing-each --backend {backend} --hypothesis-show-statistics'  # noqa
                 if len(sys.argv) >= 3 and sys.argv[2] == "main":
-                    command += " --num-examples 1000 --reuse-only --local-only"
+                    command += " --num-examples 1000 --reuse-only"
                 elif len(sys.argv) >= 3 and sys.argv[2] == "manual":
                     command += " --num-examples 1000"
                 ret = os.system(command)
