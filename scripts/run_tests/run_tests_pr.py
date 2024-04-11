aed0c114fd1cb234dc7e2db087058bd8c3e92713
Jackson McClintock <jacksondm33@gmail.com>
2024-04-10 23:42:52 -0400

    fix: make python hashing deterministic

--- scripts/run_tests/run_tests_pr.py
+++ scripts/run_tests/run_tests_pr.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                 print(f"{line[:-1]}")
                 print(f"{'*' * 100}\n")
                 sys.stdout.flush()
-                command = f'docker run --rm -v "$(pwd)":/ivy -v "$(pwd)"/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest --tb=short {test_path} --skip-trace-testing --skip-trace-testing-each --backend {backend} --derandomize --hypothesis-show-statistics'  # noqa
+                command = f'docker run --rm -v "$(pwd)":/ivy -v "$(pwd)"/.hypothesis:/.hypothesis -e "PYTHONHASHSEED=0" unifyai/ivy:latest python3 -m pytest --tb=short {test_path} --skip-trace-testing --skip-trace-testing-each --backend {backend} --derandomize --hypothesis-show-statistics'  # noqa
                 ret = os.system(command)
                 if ret != 0:
                     failed = True
