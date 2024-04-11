62473ef922b8581f35575b3badf79ae81b408d13
Jackson McClintock <jacksondm33@gmail.com>
2024-04-11 18:12:38 -0400

    feat: try example reuse again with disabled hash randomization

--- scripts/run_tests/run_tests_pr.py
+++ scripts/run_tests/run_tests_pr.py
@@ -13,7 +13,11 @@ if __name__ == "__main__":
                 print(f"{line[:-1]}")
                 print(f"{'*' * 100}\n")
                 sys.stdout.flush()
-                command = f'docker run --rm -v "$(pwd)":/ivy -v "$(pwd)"/.hypothesis:/.hypothesis -e "PYTHONHASHSEED=0" unifyai/ivy:latest python3 -m pytest --tb=short {test_path} --skip-trace-testing --skip-trace-testing-each --backend {backend} --derandomize --hypothesis-show-statistics'  # noqa
+                command = f'docker run --rm -v "$(pwd)":/ivy -v "$(pwd)"/.hypothesis:/.hypothesis -e "PYTHONHASHSEED=0" unifyai/ivy:latest python3 -m pytest --tb=short {test_path} --skip-trace-testing --skip-trace-testing-each --backend {backend} --hypothesis-show-statistics'  # noqa
+                if len(sys.argv) >= 3 and sys.argv[2] == "main":
+                    command += " --num-examples 1000 --reuse-only"
+                elif len(sys.argv) >= 3 and sys.argv[2] == "manual":
+                    command += " --num-examples 1000"
                 ret = os.system(command)
                 if ret != 0:
                     failed = True
